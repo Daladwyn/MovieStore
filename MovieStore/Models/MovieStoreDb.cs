5be8389f543124f7b064fe07a9da8b52b3b79033
Christian Levin <christian.johansson@gmail.com>
2018-05-14 23:30:02 +0200

    Databaseconnections

--- MovieStore/Models/MovieStoreDb.cs
+++ MovieStore/Models/MovieStoreDb.cs
@@ -13,6 +13,9 @@ namespace MovieStore.Models
 
         }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderRow> OrderRows { get; set; }
+        public DbSet<Movie> Movies { get; set; }
 
     }
 }
